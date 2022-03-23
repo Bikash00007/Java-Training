6ca157f1784a394b4103d12cfb248d3e4adad701
Bikash <baralbikash@yahoo.com>
2022-03-23 09:50:12 -0900

    initial commit

---
+++ src/google.java
@@ -0,0 +1,14 @@
+public class google {
+
+
+//states
+    //home
+    //google account
+    //google apps
+    //search bar
+
+//behaviour(functions)
+    //search
+    //voice search
+    //image search
+}
