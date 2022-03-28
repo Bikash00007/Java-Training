a66442b29cc8708574db83c3df5544f45e22036b
Bikash <baralbikash@yahoo.com>
2022-03-28 09:04:55 -0900

    variableAndConstant

---
+++ src/variableAndConstant.java
@@ -0,0 +1,22 @@
+public class variableAndConstant {
+    public static void min(String[] args) {
+
+        String city = "KATHMANDU";
+        String city = "POKHARA";
+
+        final String COUNTRY = "Nepal";
+        COUNTRY = "AnyName";
+
+    }
+
+
+    enum Size {
+        SMALL, MEDIUM, LARGE, EXTRA_LARGE
+    }
+    {int a=4;
+    int b=5;
+    int c=6;
+   // int d=(a+b)*c;
+    System.out.println(d);
+    }
+}
